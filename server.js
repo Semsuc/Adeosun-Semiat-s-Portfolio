ab4133f6053fd178d59440030b05cef6cd0c5892
semsuc <adeosunsemiat5003@gmail.com>
2025-05-18 06:19:12 +0100

    Add mysql2 to dependencies

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 
 const app = express();
 
+// serve everything in /public at the root URL
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
